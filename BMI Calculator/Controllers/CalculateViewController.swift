ec3969ce0fcd065ac6be343cf3cc27601fcd7cf1
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-04-21 19:48:52 -0300

    Segues and Navigation for Multi-Screen Apps

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -39,9 +39,16 @@ class CalculateViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / (height * height)
-        bmiValue = String(format: "%.1f", self)
+        bmiValue = String(format: "%.1f", bmi)
         
         self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
     }
     
 }
